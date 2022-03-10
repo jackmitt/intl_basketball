4d91887aa3065b635e2b8e96011257cd1cebe2b6
jackmitt <jjmittenthal@crimson.ua.edu>
2022-03-09 21:50:05 -0600

    debugging

--- dailyBettingScript.py
+++ dailyBettingScript.py
@@ -101,6 +101,7 @@ def updateSeasonStats(league, last_date):
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--headless")
     chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument("--window-size=1325x744")
     browser = webdriver.Chrome(executable_path=driver_path, options = chrome_options)
     browser.maximize_window()
     curDate = last_date +datetime.timedelta(days=1)
