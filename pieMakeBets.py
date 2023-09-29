247858b727bd4d0c21ebbcb1110f5b4f424ec4ec
jackmitt <jjmittenthal@crimson.ua.edu>
2023-09-29 13:48:39 -0500

    Reorganized
    also, deleted all the raspberry pie related stuff from years ago

--- pieMakeBets.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from dailyBettingScript import scrapePinnacle
-from dailyBettingScript import bet
-
-lines = scrapePinnacle(league)
-if (not lines.empty):
-    bet(league, lines)
